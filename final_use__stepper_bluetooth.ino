37584b6e25d70d32b0a10ad84aded6119cb6c904
dcl666 <71249820+dcl666@users.noreply.github.com>
2021-01-17 20:38:48 +0800

    Add files via upload

---
+++ final_use__stepper_bluetooth.ino
@@ -0,0 +1,51 @@
+#include <SoftwareSerial.h>
+#define vcc 3
+#define pls 4
+#define dir 5
+#define ena 6
+int TxD = 11;
+int RxD = 10;
+SoftwareSerial bluetooth(TxD, RxD);
+
+void setup() {
+Serial.begin(9600);       // start serial communication at 9600bps
+bluetooth.begin(9600);
+pinMode(vcc,OUTPUT);
+pinMode(pls,OUTPUT);
+pinMode(dir,OUTPUT);
+pinMode(ena,OUTPUT);
+}
+
+void loop() {
+ if (bluetooth.available()){
+  String value = bluetooth.readString();
+   if (value.toInt() == 1)
+   {
+  digitalWrite(vcc,HIGH);
+  digitalWrite(pls,HIGH);
+  digitalWrite(dir,HIGH);
+  digitalWrite(ena,HIGH);
+  tone(pls,500);
+   }
+   if (value.toInt() == 0)
+   {
+  digitalWrite(vcc,HIGH);
+  digitalWrite(pls,HIGH);
+  digitalWrite(dir,HIGH);
+  digitalWrite(ena,LOW);
+  tone(pls,500);
+    }
+   if (value.toInt() == 2)
+   {
+  digitalWrite(vcc,LOW);
+  digitalWrite(pls,HIGH);
+  digitalWrite(dir,HIGH);
+  digitalWrite(ena,HIGH);
+   }
+   }
+   }       
+   
+ 
+
+
+
